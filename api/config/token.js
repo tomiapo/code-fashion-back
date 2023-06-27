9588f25218b527f406ee1449592824cbb87added
Micaela <micaelauvilla9@gmail.com>
2023-06-27 17:43:10 -0300

    configuration jwt

--- api/config/token.js
+++ api/config/token.js
@@ -1,1 +1,13 @@
 // configuracion de JWT
+const jwt = require("jsonwebtoken");
+
+const SECRET = "secret";
+
+function generateToken(payload) {
+  const token = jwt.sign({ payload }, SECRET, { expiresIn: "2h" });
+  return token;
+}
+function validateToken(token) {
+  return jwt.verify(token, SECRET);
+}
+module.exports = { generateToken, validateToken };
