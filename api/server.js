d789fb2d65c454ff647120244150e8bbc4069129
tomiapo <tomasapochian@gmail.com>
2023-06-27 15:57:32 -0300

    server config

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const app = express();
+const PORT = 8000;
+const db = require("./config/db");
+const morgan = require("morgan");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
+// app.use("/api", routes);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+db.sync({ force: false }).then(
+  app.listen(PORT, () => {
+    console.log("Server listening at port", PORT);
+  })
+);
