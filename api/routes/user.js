36d312744a569df78ef385ab55173bdfbb120e15
Micaela <micaelauvilla9@gmail.com>
2023-06-27 22:00:57 -0300

    creation route login

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
+
 const router = express.Router();
 
 // Create User
@@ -7,6 +8,29 @@ router.post("/create-user", (req, res) => {
   User.create(req.body).then((user) => res.send(user));
 });
 
-//
+//LOGIN
+router.get("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      return res
+        .status(401)
+        .send("Correo electrónico o contraseña incorrectos.");
+    }
+    if (user.password !== password) {
+      return res
+        .status(401)
+        .send("Correo electrónico o contraseña incorrectos.");
+    }
+    res.send("Inicio de sesión exitoso.");
+  } catch (error) {
+    console.error("Error en el inicio de sesión:", error);
+    res.status(500).send("Se produjo un error en el inicio de sesión.");
+  }
+});
 module.exports = router;
