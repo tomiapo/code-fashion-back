fa0e019c494cf554f34b7c2f1b76f19a94304192
lucas <federicotoro467@gmail.com>
2023-06-28 12:39:46 -0300

    hash password

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,17 @@
 const Sequelize = require("sequelize");
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
-class User extends Sequelize.Model {}
+class User extends Sequelize.Model {
+  hash(password, salt) {
+    return bcrypt.hash(password, salt);
+  }
+  validatePassword(password) {
+    return this.hash(password, this.salt).then(
+      (newhash) => newhash === this.password
+    );
+  }
+}
 User.init(
   {
     firstname: {
@@ -38,5 +48,14 @@ User.init(
   },
   { sequelize: db, modelName: "user" }
 );
+
+User.beforeCreate((user) => {
+  const salt = bcrypt.genSaltSync(8);
+  user.salt = salt;
+
+  return user.hash(user.password, user.salt).then((hash) => {
+    user.password = hash;
+  });
+});
 
 module.exports = User;
