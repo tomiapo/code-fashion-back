b5615cbdb788de53b58ac993e2d2f6c0fcb50b11
lucas <federicotoro467@gmail.com>
2023-06-29 11:59:53 -0300

    show one product

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const showAllProducts = require("../controllers/productControllers");
+const Product = require("../models/Product");
 // The `/api/products` endpoint
 
 // Route to get-all-products using it's controller
 router.get("/", showAllProducts);
+
+router.get("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 module.exports = router;
