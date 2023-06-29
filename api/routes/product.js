daafb081539a32ae6698be6d986a47cf66ea21ad
lucas <federicotoro467@gmail.com>
2023-06-29 12:41:56 -0300

    get one product

--- api/routes/product.js
+++ api/routes/product.js
@@ -9,7 +9,7 @@ router.get("/", showAllProducts);
 
 router.get("/products/:id", async (req, res) => {
   const productId = req.params.id;
-  //
+  // se puede incluir todo esto dentro de productControllers.js
   try {
     const product = await Product.findByPk(productId);
     if (!product) {
