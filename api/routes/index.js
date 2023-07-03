bcfc1e75a4a20816e4715219c32482d4e9daae57
Micaela <micaelauvilla9@gmail.com>
2023-07-03 16:31:22 -0300

    add admin router in index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const user = require("./user");
 const product = require("./product");
 const orderHistory = require("./orderHistory");
+const admin = require("./admin.routes");
 
 // se llaman a todas la rutas definidas antes
 
 router.use("/user", user);
 router.use("/products", product);
 router.use("/orderhistory", orderHistory);
+router.use("/admin", admin);
 
 // Se importa todas en una y se la requiere en server.js
 
