42a9e5956800944cbcd35f2105cd334c9f19cc7c
Micaela <micaelauvilla9@gmail.com>
2023-07-05 16:12:45 -0300

    deleted adminService in userControllers

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,29 +1,8 @@
 const User = require("../models/User");
 const { generateToken } = require("../config/token");
-const AdminService = require("../services/admin.services");
+const UserService = require("../services/user.services");
 
 class UserController {
-  static async getAll(req, res) {
-    try {
-      const users = await AdminService.getAllUsers();
-      if (users.length === 0) {
-        return res.status(404).json({ error: "No users found" });
-      }
-      return res.json(users);
-    } catch (error) {
-      res.status(404).json({ error: error.message });
-    }
-  }
-  static async deleteUser(req, res) {
-    try {
-      const { id } = req.params;
-      const user = await AdminService.deleteUser(id);
-      await user.destroy();
-      return res.status(200).json({ message: "User deleted successfully" });
-    } catch (error) {
-      res.status(404).json({ error: error.message });
-    }
-  }
   static async checkoutPurchase(req, res) {
     console.log(req.body);
   }
@@ -75,6 +54,7 @@ class UserController {
   static logoutUser(req, res) {
     return res.clearCookie("authToken").sendStatus(204);
   }
+
   static async editUser(req, res) {
     try {
       const userId = req.params.userId;
