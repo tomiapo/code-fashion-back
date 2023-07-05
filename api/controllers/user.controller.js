441f414a4e8f016a7dfbc976c8ad9c4b488e3cd8
tomiapo <tomasapochian@gmail.com>
2023-07-05 13:19:27 -0300

    move logic to controller

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,6 @@
+const User = require("../models/User");
+const { generateToken } = require("../config/token");
+
 class UserController {
   static async getAll(req, res) {
     try {
@@ -22,6 +25,54 @@ class UserController {
   }
   static async checkoutPurchase(req, res) {
     console.log(req.body);
+  }
+
+  static async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      return res.status(201).send(user);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
+
+  static async loginUser(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        return res
+          .status(401)
+          .send("Correo electrónico o contraseña incorrectos.");
+      }
+      const isValid = await user.validatePassword(password);
+      if (!isValid) {
+        return res.sendStatus(401);
+      } else {
+        const payload = {
+          name: user.first_name,
+          lastname: user.last_name,
+          email: user.email,
+          id: user.id,
+          address: user.address,
+        };
+        const token = generateToken(payload);
+        res.cookie("authToken", token);
+        res.send({ user: payload, token: token });
+      }
+    } catch (error) {
+      return res
+        .status(500)
+        .send("Se produjo un error en el inicio de sesión.");
+    }
+  }
+
+  static logoutUser(req, res) {
+    return res.clearCookie("authToken").sendStatus(204);
   }
 }
 module.exports = UserController;
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,54 +1,11 @@
 const express = require("express");
-const User = require("../models/User");
-const { generateToken } = require("../config/token");
-const validateUser = require("../middlewares/validateUser");
-
+const UserController = require("../controllers/user.controller");
 const router = express.Router();
 
-router.post("/create-user", (req, res) => {
-  User.create(req.body).then((user) => res.send(user));
-});
+router.post("/create-user", UserController.createUser);
 
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post("/login", UserController.loginUser);
 
-    const user = await User.findOne({
-      where: { email },
-    });
-
-    if (!user) {
-      return res
-        .status(401)
-        .send("Correo electrónico o contraseña incorrectos.");
-    }
-    const isValid = await user.validatePassword(password);
-    if (!isValid) {
-      return res.sendStatus(401);
-    } else {
-      const payload = {
-        name: user.first_name,
-        lastname: user.last_name,
-        email: user.email,
-        id: user.id,
-        address: user.address,
-      };
-      const token = generateToken(payload);
-      res.cookie("authToken", token);
-      res.send({ user: payload, token: token });
-    }
-  } catch (error) {
-    console.error("Error en el inicio de sesión:", error);
-    res.status(500).send("Se produjo un error en el inicio de sesión.");
-  }
-});
-
-router.get("/me", validateUser, (req, res) => {
-  res.send(req.user);
-});
-
-router.post("/logout", (req, res) => {
-  res.clearCookie("authToken").sendStatus(204);
-});
+router.post("/logout", UserController.logoutUser);
 
 module.exports = router;
