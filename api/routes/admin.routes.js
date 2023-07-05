610f206905349ddbcbbe6d48ae3b1af9df0f1116
Micaela <micaelauvilla9@gmail.com>
2023-07-05 14:16:51 -0300

    bugfix admin

--- api/routes/admin.routes.js
+++ api/routes/admin.routes.js
@@ -2,8 +2,28 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const AdminController = require("../controllers/admin.controllers");
+const { validateToken } = require("../config/token");
 
-router.get("/users", AdminController.getAll);
+function authenticate(req, res, next) {
+  const token = req.cookies.authToken;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "No se proporcionó un token de autenticación" });
+  }
+
+  try {
+    const decoded = validateToken(token);
+
+    req.user = decoded.payload;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token inválido" });
+  }
+}
+router.get("/users", authenticate, AdminController.getAll);
 router.delete("/users/:id", AdminController.deleteUser);
 
 module.exports = router;
