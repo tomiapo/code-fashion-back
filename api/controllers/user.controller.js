2302a2a1b6ad80f4df375891253a03562d0fd86e
Micaela <micaelauvilla9@gmail.com>
2023-07-05 14:38:10 -0300

    delete user.controllers

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -74,5 +74,18 @@ class UserController {
   static logoutUser(req, res) {
     return res.clearCookie("authToken").sendStatus(204);
   }
+  static async editUser(req, res) {
+    try {
+      const userId = req.params.userId;
+      const userData = req.body;
+      const usersModify = await UserService.updateUser(userId, userData);
+      if (usersModify.length === 0) {
+        return res.status(404).json({ error: "No user found" });
+      }
+      return res.json(usersModify);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
+  }
 }
 module.exports = UserController;
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,17 +3,12 @@ const express = require("express");
 const User = require("../models/User");
 const { generateToken } = require("../config/token");
 const validateUser = require("../middlewares/validateUser");
-const UserController = require("../controllers/user.controllers");
-
-
-
+const UserController = require("../controllers/user.controller");
 
 const router = express.Router();
 router.put("/:userId", UserController.editUser);
 
-
 router.post("/create-user", UserController.createUser);
-
 
 router.post("/login", UserController.loginUser);
 
