4dc774040873fd2914b4389c77707ffa3fbb623c
Micaela <micaelauvilla9@gmail.com>
2023-07-06 11:38:34 -0300

    add username to cookies

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -39,7 +39,9 @@ class UserController {
           email: user.email,
           id: user.id,
           address: user.address,
-          isSeller: user.is_seller,
+          is_seller: user.is_seller,
+          username: user.username,
+          is_super_admin: user.is_super_admin,
         };
         const token = generateToken(payload);
 
