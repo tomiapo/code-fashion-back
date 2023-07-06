bd3e625d41cdaa578c603d0c09a1ef04e68b074b
lucas <federicotoro467@gmail.com>
2023-07-05 23:53:04 -0300

    create model review

--- api/models/index.js
+++ api/models/index.js
@@ -1,7 +1,14 @@
 const User = require("./User");
 const Product = require("./Product");
 const OrderHistory = require("./OrderHistory");
+const Review = require("./Review");
 
 OrderHistory.belongsTo(User, { foreignKey: "user_id" });
+
+User.hasMany(Review, { foreignKey: "user_id" });
+Review.belongsTo(User, { foreignKey: "user_id" });
+
+Product.hasMany(Review, { foreignKey: "product_id" });
+Review.belongsTo(Product, { foreignKey: "product_id" });
 
 module.exports = { OrderHistory, Product, User };
